ea45ffcfc81a4589169a444abb6dc301ff6c0af2
Edgar Moreno Ramirez <edgar_mramirez@live.com.mx>
2021-12-28 00:49:43 -0600

    Segundo commit del index.php

--- index.php
+++ index.php
@@ -2,9 +2,11 @@
 
 ini_set('display_errors', '1');
 ini_set('error_reporting', E_ALL);
+
 require_once "models/enlaces.php";
 require_once "controllers/controller.php";
 
 $mvc = new MvcController();
 $mvc -> pagina();
+
 ?>
