1c3321a6827f633531c21752b46629e865e9f0a4
Edgar Moreno Ramirez <edgar_mramirez@live.com.mx>
2021-12-28 00:46:58 -0600

    Primer commit del index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_errors', '1');
+ini_set('error_reporting', E_ALL);
+require_once "models/enlaces.php";
+require_once "controllers/controller.php";
+
+$mvc = new MvcController();
+$mvc -> pagina();
+?>
