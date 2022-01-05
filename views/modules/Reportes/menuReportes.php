cc5b31b641efccfd3b60f1f26f93884b35ba11d6
Edgar Moreno Ramirez <edgar_mramirez@live.com.mx>
2022-01-04 22:28:22 -0600

    SE AGREGA EL BOTON DE CORTE DE CAJA CLIENTE, Y SE COMENTA CON LIENAS UN DUPLICADO

--- views/modules/Reportes/menuReportes.php
+++ views/modules/Reportes/menuReportes.php
@@ -3,14 +3,19 @@
         <div class="col-md-2">
             <a class="btn btn-success" href="index.php?action=Reportes/listadoVentasMesas">Ventas a Mesas</a>
         </div>
+        <!--
         <div class="col-md-2">
             <a class="btn-update" href="index.php?action=Reportes/listadoVentasClientes">Ventas a Cliente</a>
         </div> 
+        -->
         <div class="col-md-2">
             <a class="btn btn-danger" href="index.php?action=Reportes/listadoVentasClientes">Ventas Por Cliente</a>
         </div> 
         <div class="col-md-2">
-            <a class="btn btn-info text-white" href="index.php?action=Reportes/listadoVentasClientes">Corte del Día</a>
+            <a class="btn btn-info text-white" onclick="corteDelDiaMesas()">Corte Caja Mesas</a>
+        </div> 
+        <div class="col-md-2">
+            <a class="btn-update" onclick="corteDelDiaClientes()">Corte Caja Clientes</a>
         </div> 
     </div>
 </div>
