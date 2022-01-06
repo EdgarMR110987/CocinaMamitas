5d4ff8bb41362fd978c5008298a33a1f9a03db08
Edgar Moreno Ramirez <edgar_mramirez@live.com.mx>
2022-01-06 00:48:08 -0600

    Se crea el menu de navegación para los clientes

---
+++ views/navegacionCliente.php
@@ -0,0 +1,17 @@
+<nav class="navbar fixed-top navbar-dark bg-dark pt-0 pb-0">
+
+    <a href="index.php?action=Clientes/listadoVentasCliente" class="navbar-brand">
+        <span>
+            <img class="img-auto" src="views/img/ventas.gif" alt="">
+        </span>
+        Mis Cuentas
+    </a>
+    
+    <a href="#" onclick="salir()" class="navbar-brand">
+        <span>
+            <img class="img-auto" src="views/img/Exit.gif" alt="">
+        </span>
+        Salir
+    </a>
+
+</nav>
